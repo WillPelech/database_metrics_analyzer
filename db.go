81b072b3384002435f3da4d21dc3161a7ee04146
WillScooby <wpelech23@gmail.com>
2025-11-27 21:31:15 -0700

    creating configs to read json

--- db.go
+++ db.go
@@ -7,7 +7,22 @@ import (
 	"log"
 )
 
+type postgres_config struct{
+	Database struct {
+		Host						string 'json:"host"'
+		Port						string  'json:"port"'
+		AdminUsername						string  'json:"admin_username"'
+		Adminpassword						string  'json:"admin_password"'
+	}'json:database'
+	
+}
 func main() {
-	conn, err := pgx.Conne
+	postgres_json, err := os.Open("postgres_config_file.json")
+	if err != nil  {
+		fmt.Println(err)
+	}
+	var postgres_cfg postgres_config
+	:
+
 
 }
