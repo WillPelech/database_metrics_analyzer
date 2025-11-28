71d3d641e7ffbf34586a80585db451dc7b9cbaae
WillScooby <wpelech23@gmail.com>
2025-11-27 21:51:52 -0700

    read the json file

--- db.go
+++ db.go
@@ -1,28 +1,41 @@
 package main
 
 import (
-	"context"
+	"encoding/json"
 	"fmt"
-	"github.com/jackc/pgx/v5"
-	"log"
+	"os"
 )
 
-type postgres_config struct{
+// import (
+//
+//	"context"
+//	"encoding/json"
+//	"fmt"
+//	"github.com/jackc/pgx/v5"
+//	"log"
+//
+// )
+type postgres_config struct {
 	Database struct {
-		Host						string 'json:"host"'
-		Port						string  'json:"port"'
-		AdminUsername						string  'json:"admin_username"'
-		Adminpassword						string  'json:"admin_password"'
-	}'json:database'
-	
+		Host          string `json:"host"`
+		Port          string `json:"port"`
+		AdminUsername string `json:"admin_username"`
+		Adminpassword string `json:"admin_password"`
+	} `json:"database"`
 }
+
 func main() {
-	postgres_json, err := os.Open("postgres_config_file.json")
-	if err != nil  {
+	postgres_json, err := os.Open("postgres_config.json")
+	if err != nil {
 		fmt.Println(err)
 	}
 	var postgres_cfg postgres_config
-	:
 
+	decoder := json.NewDecoder(postgres_json)
+	if err := decoder.Decode(&postgres_cfg); err != nil {
+		panic(err)
+	}
+	fmt.Println("successfully read JSON")
+	fmt.Println("Host", postgres_cfg.Database.Host)
 
 }
